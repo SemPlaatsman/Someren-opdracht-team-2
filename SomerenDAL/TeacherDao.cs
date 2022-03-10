bed990cf3e4204e2d5f17619b63ca3a18e093396
Floortje Tjeertes <43434642+floriantjeertes@users.noreply.github.com>
2022-03-10 22:37:07 +0100

    updates

---
+++ SomerenDAL/TeacherDao.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Collections.ObjectModel;
+using SomerenModel;
+
+namespace SomerenDAL
+{
+    public class TeacherDao : BaseDao
+    {      
+        public List<Teacher> GetAllTeachers()
+        { 
+            string query = "SELECT teacherId, teacherFirstName, teacherLastName FROM Teachers";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+
+        private List<Teacher> ReadTables(DataTable dataTable)
+        {
+            List<Teacher> students = new List<Teacher>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                Teacher student = new Teacher()
+                {
+                    Number = (int)dr["studentID"],
+                    FirstName = (string)dr["studentFirstName"],
+                    LastName = (string)dr["studentLastName"]
+
+
+                };
+                students.Add(student);
+            }
+            return students;
+        }
+    }
+}
